6675a14c2438f9ec33c2309828edc876c3ebb7e5
yanwen-19 <1103951643@qq.com>
2026-05-28 19:49:02 +0800

    Update settings.gradle.kts

--- DailyCostApp_Final/settings.gradle.kts
+++ DailyCostApp_Final/settings.gradle.kts
@@ -1,10 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
 plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
 }
 
-@Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
